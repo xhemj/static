320b87b8cbb720e9451d4399042a7b2b712c0916
xhemj <55266496+xhemj@users.noreply.github.com>
2020-04-23 15:28:54 +0800

    Create footer.js

---
+++ xhemj-footer/base/footer.js
@@ -0,0 +1,26 @@
+/*
+BY:Xhemj
+©2020 Xhemj Footer
+https://xhemj.gitee.io/
+*/
+document.write("<link href=\"https:\/\/xhemj.gitee.io\/p\/static\/bootstrap.min.css\" rel=\"stylesheet\"\/>");
+document.write("<meta http-equiv=\"Content-Type\" content=\"text\/html; charset=UTF-8\">");
+document.write("<style>html{height:100%}body{height:100%}a{text-decoration:none!important;color:black}.contentContainer{min-height:calc(100% - 3em)}.footer{text-align:center;line-height:2em}@media(max-width:1024px){body{padding-left:0;padding-right:0}.contentContainer{min-height:calc(100% - 3em)}}<\/style>");
+document.write("<meta name=\"author\" content=\"xhemj\">");
+document.write("<div class=\"footer row-fluid\"><div>");
+document.write("<hr align=center width=\"auto\"\/");
+document.write("<div id=\"social-share\" style=\"text-align: center;\">");
+document.write("<link rel=\"stylesheet\" href=\"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/social-share.js\/1.0.16\/css\/share.min.css\">");
+document.write("<div class=\"social-share\" data-sites=\"qq,qzone,wechat,weibo,facebook,twitter\" data-image=\"https:\/\/xhemj.gitee.io\/logo.png\" data-wechat-qrcode-title=\"分享\" data-wechat-qrcode-helper=\"使用微信扫一扫分享\"><\/div>");
+document.write("<script type=\"text\/javascript\" src=\"https:\/\/cdnjs.cloudflare.com\/ajax\/libs\/social-share.js\/1.0.16\/js\/social-share.min.js\"><\/script>");
+document.write("<\/div>");
+document.write("<strong>© 2020 Xhemj<\/strong>");
+document.write("&nbsp;|&nbsp;<a href=\"\/\" target=\"_blank\">官网<\/a>");
+document.write("&nbsp|&nbsp;<a href=\"https:\/\/xhemj.gitee.io\/oneQRCodeforPay\/\" target=\"_blank\">赞助本网站<\/a>");
+document.write("&nbsp|&nbsp;<a href=\"https:\/\/gitee.com\/xhemj\/xhemj\/\" target=\"_blank\">Gitee<\/a>");
+document.write("&nbsp|&nbsp;<a href=\"mailto:xhemj2680@163.com\" target=\"_blank\">联系我<\/a><\/br>");
+document.write("<img src=\"https:\/\/xhemj.gitee.io\/logo.svg\" width=\"auto\" height=\"50\"><strong>BY: X H E M J<\/strong><\/br>");
+document.write("<strong>\"这只是Xhemj的网站而已，也没有什么别的！\"<\/strong>");
+document.write("<div id=\"cc-myssl-id\">");
+document.write("<a href=\"https:\/\/myssl.com\/xhemj.gitee.io?from=mysslid\" target=\"_blank\"><img src=\"https:\/\/static.myssl.com\/res\/images\/myssl-id3.png\" alt=\"\" style=\"max-height:50px;display:block;margin:0 auto\"><\/a>");
+document.write("<\/div>");
