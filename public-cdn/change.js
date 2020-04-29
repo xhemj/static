a41d0b0c629daa15988e8a8c491e6c8a97df46d8
xhemj <55266496+xhemj@users.noreply.github.com>
2020-04-29 08:27:34 +0800

    Create change.js

---
+++ public-cdn/change.js
@@ -0,0 +1,22 @@
+//Jsdelivr
+<script src="https://cdn.jsdelivr.net/npm/docsify/lib/docsify.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/docsify/lib/plugins/search.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/docsify/lib/plugins/zoom-image.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/docsify-copy-code"></script>
+<script src="https://cdn.jsdelivr.net/npm/docsify/lib/plugins/emoji.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/xhemj/xhemj.github.io@master/backup/js/docsify-pagination.min.js"></script>
+<script src="https://unpkg.com/docsify-footer-enh/dist/docsify-footer-enh.min.js"></script>
+<script src="https://unpkg.com/docsify-remote-markdown/dist/docsify-remote-markdown.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/docsify/lib/plugins/external-script.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/827652549/docsify-count@master/dist/countable.min.js"></script>
+//cdnjs
+<script src="https://cdnjs.cloudflare.com/ajax/libs/docsify/4.11.2/docsify.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/docsify/4.11.2/plugins/search.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/docsify/4.11.2/plugins/zoom-image.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/docsify/4.11.2/plugins/emoji.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/docsify/4.11.2/plugins/external-script.min.js"></script>
+<script src="https://unpkg.com/docsify-remote-markdown/dist/docsify-remote-markdown.min.js"></script>
+<script src="https://unpkg.com/docsify-footer-enh/dist/docsify-footer-enh.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/827652549/docsify-count@master/dist/countable.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/xhemj/xhemj.github.io@master/backup/js/docsify-pagination.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/docsify-copy-code"></script>
