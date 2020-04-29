cc1fd99cb22da629faf0f5f8745131596d7ce8ab
xhemj <55266496+xhemj@users.noreply.github.com>
2020-04-29 09:05:04 +0800

    Update all.js

--- public-cdn/all.js
+++ public-cdn/all.js
@@ -29,4 +29,4 @@ if(cdnuse == "3"){
 <script src="https://cdn.jsdelivr.net/combine/npm/docsify/lib/docsify.min.js,npm/docsify/lib/plugins/search.min.js,npm/docsify/lib/plugins/zoom-image.min.js,npm/docsify-copy-code,npm/docsify/lib/plugins/emoji.min.js,gh/xhemj/xhemj.github.io@master/backup/js/docsify-pagination.min.js,gh/xhemj/xhemj.github.io@master/backup/js/docsify-footer-enh.min.js,gh/xhemj/xhemj.github.io@master/backup/js/docsify-remote-markdown.min.js,npm/docsify/lib/plugins/external-script.min.js,gh/827652549/docsify-count@master/dist/countable.min.js">
 }
 //备用cdn库
-<script src="https://cdn.bootcdn.net/ajax/libs/docsify/4.11.2/docsify.js"></script>
+//<script src="https://cdn.bootcdn.net/ajax/libs/docsify/4.11.2/docsify.js"></script>
