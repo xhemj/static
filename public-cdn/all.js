6c6529a94649a13c466945c7e164e0a7215ba046
xhemj <55266496+xhemj@users.noreply.github.com>
2020-04-29 09:03:00 +0800

    Update all.js

--- public-cdn/all.js
+++ public-cdn/all.js
@@ -6,8 +6,8 @@ if(cdnuse == "1"){
 <script src="https://cdn.jsdelivr.net/npm/docsify-copy-code"></script>
 <script src="https://cdn.jsdelivr.net/npm/docsify/lib/plugins/emoji.min.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/xhemj/xhemj.github.io@master/backup/js/docsify-pagination.min.js"></script>
-<script src="https://unpkg.com/docsify-footer-enh/dist/docsify-footer-enh.min.js"></script>
-<script src="https://unpkg.com/docsify-remote-markdown/dist/docsify-remote-markdown.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/xhemj/xhemj.github.io@master/backup/js/docsify-footer-enh.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/xhemj/xhemj.github.io@master/backup/js/docsify-remote-markdown.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/docsify/lib/plugins/external-script.min.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/827652549/docsify-count@master/dist/countable.min.js"></script>
 }
@@ -23,6 +23,10 @@ if(cdnuse == "2"){
 <script src="https://cdn.jsdelivr.net/gh/827652549/docsify-count@master/dist/countable.min.js"></script>
 <script src="https://cdn.jsdelivr.net/gh/xhemj/xhemj.github.io@master/backup/js/docsify-pagination.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/docsify-copy-code"></script>
+}
+if(cdnuse = "3"){
+//使用Jsdelivr压缩合并功能
+<script src="https://cdn.jsdelivr.net/combine/npm/docsify/lib/docsify.min.js,npm/docsify/lib/plugins/search.min.js,npm/docsify/lib/plugins/zoom-image.min.js,npm/docsify-copy-code,npm/docsify/lib/plugins/emoji.min.js,gh/xhemj/xhemj.github.io@master/backup/js/docsify-pagination.min.js,gh/xhemj/xhemj.github.io@master/backup/js/docsify-footer-enh.min.js,gh/xhemj/xhemj.github.io@master/backup/js/docsify-remote-markdown.min.js,npm/docsify/lib/plugins/external-script.min.js,gh/827652549/docsify-count@master/dist/countable.min.js">
 }
 //备用cdn库
 <script src="https://cdn.bootcdn.net/ajax/libs/docsify/4.11.2/docsify.js"></script>
