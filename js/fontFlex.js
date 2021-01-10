b1c20062daa9c463d20ae59cb9728752336d889a
xhemj <xhemj2680@163.com>
2021-01-10 20:27:44 +0800

    Create fontFlex.js

---
+++ js/fontFlex.js
@@ -0,0 +1,1 @@
+!function(n){n.fn.fontFlex=function(i,e,r){var t=this;n(window).resize((function(){var n=window.innerWidth/r;n<i&&(n=i),n>e&&(n=e),t.css("font-size",n+"px")})).trigger("resize")}}(jQuery);
